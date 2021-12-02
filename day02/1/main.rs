86abd8e49931e74405ebcbb057e3b5d2da8d7a0b
Dawid Bialy <dawid_bialy@tvn.pl>
2021-12-02 16:34:28 +0100

    day2 part 1

---
+++ day02/1/main.rs
@@ -0,0 +1,46 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::str;
+
+const FILENAME: &str = "day02/1/input.txt";
+const DIRECTION_FORWARD: &str = "forward";
+const DIRECTION_DOWN: &str = "down";
+const DIRECTION_UP: &str = "up";
+
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+
+impl Position {
+    fn run(&mut self, direction: &str, steps: i32) {
+        match direction {
+            DIRECTION_DOWN => self.y += steps,
+            DIRECTION_UP => self.y -= steps,
+            DIRECTION_FORWARD => self.x += steps,
+            _ => {},
+        }
+    }
+
+    fn new(x: i32, y: i32) -> Position {
+        Position { x: x, y: y }
+    }
+}
+
+
+fn main() {
+    let file = File::open(FILENAME).unwrap();
+    let reader = BufReader::new(file);
+    let mut pos = Position::new(0, 0);
+
+    for (_index, line) in reader.lines().enumerate() {
+        let l = line.unwrap();
+        let mut s = l.split_whitespace();
+        pos.run(s.next().unwrap(), s.next().unwrap().parse::<i32>().unwrap());
+    }
+   
+
+    println!("pos:[x:{}, y:{}]", pos.x, pos.y);
+    println!("Result: {}", pos.x * pos.y)
+}
